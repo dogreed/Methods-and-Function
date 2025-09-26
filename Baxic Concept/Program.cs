bab72276c5827e67a8a1462123e2e9ea7e21560c
USER <dogreed69@gmail.com>
2025-09-26 18:51:08 +0545

    Add project files.

---
+++ Baxic Concept/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Globalization;
+
+namespace he
+{
+	class Look {
+
+		static void info(string name)
+		{
+
+			Console.WriteLine("hello!!" + name );
+		}
+	static void Main() { 
+		
+		//methods with parameters
+		string name = "John";
+
+			info(name);
+		}	
+	}
+}
