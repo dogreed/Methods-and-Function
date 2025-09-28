815c12455013a89e9de8bb11e89d30a3106670b8
USER <dogreed69@gmail.com>
2025-09-28 19:58:39 +0545

    ah leannig the basix concept of method practically

--- Baxic Concept/Program.cs
+++ Baxic Concept/Program.cs
@@ -1,20 +1,55 @@
-﻿using System.Globalization;
+﻿using System.Diagnostics;
+using System.Globalization;
+using System.Reflection.Metadata.Ecma335;
 
 namespace he
 {
-	class Look {
+	class Look
+	{
 
-		static void info(string name)
+		// calcualtor usinmg methods, lets check the prime number
+
+		static void primenumberchecker(int num)
+		{
+			bool isprimenumber = true;
+			if (num <= 1)
+			{
+				isprimenumber = false;
+			}
+			else
+			{
+
+				for (int i = 2; i <= num / 2; i++)
+				{
+
+					if (num % i == 0)
+					{
+						isprimenumber = false;
+						break;
+					}
+
+				}
+			}
+			if (isprimenumber)
+			{
+				Console.WriteLine("its a prime number " + num);
+			}
+			else
+			{
+				Console.WriteLine("its not a prime number" + num);
+			}
+		}
+		
+
+		public static void Main(string[] args)
 		{
 
-			Console.WriteLine("hello!!" + name );
+			Console.WriteLine("Prime Number Checker");
+			Console.WriteLine("Enter a number:");
+			int num1 = Convert.ToInt32(Console.ReadLine());
+			Console.WriteLine("ayoooo");
+			primenumberchecker(num1);
 		}
-	static void Main() { 
-		
-		//methods with parameters
-		string name = "John";
-
-			info(name);
-		}	
 	}
+
 }
